667d71d3e26c21c216403f361519e98551eb9f7c
muskan <atul3004@gmail.com>
2026-04-10 17:51:58 +0300

    bootstraping

--- survey/src/java/com/kiptoo2000/survey/persistence/JpaUtility.java
+++ survey/src/java/com/kiptoo2000/survey/persistence/JpaUtility.java
@@ -4,12 +4,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public final class JpaUtil {
+public final class JpaUtility {
 
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY =
             Persistence.createEntityManagerFactory("surveyPU");
 
-    private JpaUtil() {
+    private JpaUtility() {
     }
 
     public static EntityManager createEntityManager() {
